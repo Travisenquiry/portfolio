8a062eb53e46ab4896a407b89adc8f6ac54002e5
travisenquiry <travisenquiry@gmail.com>
2020-06-23 14:01:28 +0800

    Updates

---
+++ precache-manifest.d834575a1c4587c84a433df767a56487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0a03223ba58772b9271c81a8f44018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a8dde8340645a306e8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a8dde8340645a306e8",
+    "url": "/static/js/main.ba3eb5a5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
