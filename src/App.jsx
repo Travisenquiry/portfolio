f9c18439729ccd4dada382e41d7bc8d5188fd1ab
travisenquiry <travisenquiry@gmail.com>
2020-06-23 18:26:52 +0800

    added carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import CarouselComponent from './carouselComponent';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        hello
+        <h1 className="center">Travis's Portfolio</h1>
+        <div>
+          <CarouselComponent />
+        </div>
       </div>
     </div>
   );
